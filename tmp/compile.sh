37e4b5f9058acb6dc57093449cad442285f0df8f
BalakeKarbon <blakekarbon@gmail.com>
2025-10-03 14:49:43 +0000

    Some more debugging and expirimenting in prep.

--- tmp/compile.sh
+++ tmp/compile.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-cobc -x -C mainprog.cob -K SUBPROG
-cobc -C subprog.cob
+# This is for continued non split line programs
+#cobc -v -fformat=variable -C subprog.cob -K emscripten_run_script
+#cobc -v -fformat=variable -x -C mainprog.cob -K SUBPROG
+# This is for split line programs that are standard cobol.
+cobc -v -C subprog.cob -K emscripten_run_script
+cobc -v -x -C mainprog.cob -K SUBPROG
 find . -type f -name '*.c' -exec sed -i '/module->module_cancel\.funcptr =/ s/^/\/\//' {} +
+emcc -o prog.js mainprog.c subprog.c -lgmp -lcob -s STANDALONE_WASM=1
 #emcc -g \
 #  -s ASSERTIONS=2 \
 #  -s EXPORTED_FUNCTIONS="['_SUBPROG', '_cob_stop_run', '_cob_init']" \
 #  -s STANDALONE_WASM=1 \
 #  -o prog.js mainprog.c subprog.c -lgmp -lcob -s STANDALONE_WASM=1
-emcc -o prog.js mainprog.c subprog.c -lgmp -lcob -s STANDALONE_WASM=1
 #emcc -O0 -g4 -o prog.html mainprog.c subprog.c -lgmp -lcob
 #emcc -o prog.html mainprog.c subprog.c -lgmp -lcob
 #emcc -o prog.js mainprog.c subprog.c -lgmp -lcob -s STANDALONE_WASM=1 -Wbad-function-cast -Wcast-function-type
